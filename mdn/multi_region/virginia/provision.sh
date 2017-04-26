b6d3a52e9694e4a0d0c7d87ddc1c86b3445c6fd7
Dave Parfitt <dparfitt@mozilla.com>
2017-04-26 18:44:37 +0000

    cleanup

--- mdn/multi_region/virginia/provision.sh
+++ mdn/multi_region/virginia/provision.sh
@@ -5,10 +5,6 @@ export MDN_PROVISIONING_REGION="us-east-1"
 export TF_VAR_region="${MDN_PROVISIONING_REGION}"
 export TF_VAR_subnets="subnet-43125f6e,subnet-a699aaef,subnet-b6ceb6ed"
 
-#export KOPS_NAME="virginia.moz.works"
-#export TF_VAR_vpc_id="vpc-1b35a07d"
-
 # Apply Terraform
 cd ../tf && ./common.sh
-# -var-file $SNIPPETS_VARFILE \
 
