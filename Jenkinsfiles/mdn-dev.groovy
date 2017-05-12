33f6954598b288b84b724f4c52deed287beb69a0
Josh Mize <jgmize@gmail.com>
2017-05-12 19:10:56 +0000

    WIP

--- Jenkinsfiles/mdn-dev.groovy
+++ Jenkinsfiles/mdn-dev.groovy
@@ -1,8 +1,5 @@
-stage('kubectl apply') {
+stage('virginia') {
   sh 'kubectl config use-context virginia'
-  env.GIT_COMMIT_SHORT = 'a3a53b7'
-  env.MEMCACHED_RELEASE = 'mdn-dev'
-  env.MYSQL_RELEASE = 'manageable-puffin'
   //TODO use j2 from docker to eliminate system dependency
-  sh 'j2 mdn/k8s/mdn-dev.yaml.jinja | kubectl apply -n mdn-dev -f -'
+  sh '. virginia-env && j2 mdn/k8s/mdn-dev.yaml.jinja | kubectl apply -n mdn-dev -f -'
 }
