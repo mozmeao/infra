e944b15b606af1a4ae9662d220f7b1b27ca43201
Dave Parfitt <diparfitt@gmail.com>
2017-06-15 11:25:13 -0400

    cleanup

--- apps/snippets-stats/setup.sh
+++ apps/snippets-stats/setup.sh
@@ -11,11 +11,7 @@ if [ -z "$KUBECONFIG" ]; then
     exit 1
 fi
 
-
 deis create snippets-stats --no-remote
-
 deis pull mozmeao/snippets-stats:37727e -a snippets-stats
-
 deis config:set ALLOWED_HOSTS=\* -a snippets-stats
-
 kubectl -n snippets-stats apply -f ./snippets-stats-prod-nodeport.yaml
