7afc851412d27f9261cd0777106622d87c0027eb
Josh Mize <jgmize@gmail.com>
2017-06-22 14:59:04 -0500

    Comment out failing helm install command

--- jenkins/mdn-dev.groovy
+++ jenkins/mdn-dev.groovy
@@ -23,8 +23,9 @@ stage('tokyo') {
     env.GIT_COMMIT_SHORT = 'a3a53b7'
     //TODO use j2 from docker to eliminate system dependency
     sh 'j2 apps/mdn/k8s/mdn-dev.yaml.jinja | kubectl apply -n mdn-dev -f -'
-    sh 'helm install --namespace $NAMESPACE --name $MEMCACHED_RELEASE --reuse-name stable/memcached'
+    //sh 'helm install --namespace $NAMESPACE --name $MEMCACHED_RELEASE --reuse-name stable/memcached'
     //sh 'helm install --namespace $NAMESPACE --name $MYSQL_RELEASE --reuse-name --set persistence.size=40Gi stable/mysql '
+    //TODO: fix commands above; failing with "Error: unknown flag: --reuse-name"
   } catch(err) {
     sh "bin/irc-notify.sh --stage tokyo --status failed"
     throw err
