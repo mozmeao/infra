b5e48290db0203f361145e7a3cb76a5b4f1404fa
Josh Mize <jgmize@users.noreply.github.com>
2017-07-10 09:00:50 -0500

    Provision s3 + cloudfront for assets.moz.works (#330)

---
+++ apps/assets/tf/assets.tf
@@ -0,0 +1,133 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "assets-provisioning-tf-state"
+    key    = "tf-state"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "assets-moz-works-logs"
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket" "assets-moz-works" {
+  bucket = "assets-moz-works"
+  region = "${var.region}"
+  acl    = "log-delivery-write"
+
+  force_destroy = ""
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
+  hosted_zone_id = "${lookup(var.hosted-zone-id-defs, var.region)}"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.id}"
+    target_prefix = "logs/"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  website_domain   = "s3-website-${var.region}.amazonaws.com"
+  website_endpoint = "assets-moz-works.s3-website-${var.region}.amazonaws.com"
+
+  versioning {
+    enabled = true
+  }
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "assets-moz-works policy",
+  "Statement": [
+    {
+      "Sid": "Assets-Moz-WorksAllowListBucket",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::assets-moz-works"
+    },
+    {
+      "Sid": "Assets-Moz-WorksAllowIndexDotHTML",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::assets-moz-works/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "assets-moz-works.s3-website-us-west-2.amazonaws.com"
+    origin_id   = "Assets-Moz-Works"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "No comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "assets-moz-works-logs.s3.amazonaws.com"
+    prefix          = "cflogs"
+  }
+
+  aliases = ["assets.moz.works"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "Assets-Moz-Works"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 60
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:236517346949:certificate/500bd7d7-90bb-46b4-b0dc-8b31eea030d4"
+    ssl_support_method  = "sni-only"
+
+    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
+    minimum_protocol_version = "TLSv1"
+  }
+}
+
