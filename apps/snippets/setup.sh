229b94df928f25ca7e7c502b923c04615268992e
Dave Parfitt <dparfitt@mozilla.com>
2017-07-13 20:02:04 +0000

    add snippets domain

--- apps/snippets/setup.sh
+++ apps/snippets/setup.sh
@@ -7,6 +7,8 @@ wget https://raw.githubusercontent.com/mozmar/snippets-service/master/Procfile
 deis create snippets-prod --no-remote
 kubectl -n snippets-prod apply -f ./k8s/snippets-prod-nodeport.yaml
 
+deis domains:add snippets.mozilla.com -a snippets-prod
+
 deis config:set ALLOWED_HOSTS=snippets-prod.frankfurt,snippets-prod.moz.works,snippets.cdn.mozilla.net,snippets-prod-cdn.moz.works,snippets.mozilla.com -a snippets-prod
 
 deis pull mozorg/snippets:9973b1 -a snippets-prod
