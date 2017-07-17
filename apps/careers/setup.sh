b5d59e1ba9c8c8514c84ca24a70976c4d60e97b3
Dave Parfitt <dparfitt@mozilla.com>
2017-07-17 16:02:48 +0000

    update careers info

--- apps/careers/setup.sh
+++ apps/careers/setup.sh
@@ -7,6 +7,6 @@ wget https://raw.githubusercontent.com/mozmar/lumbergh/master/Procfile
 
 deis create careers-prod --no-remote
 deis config:set ALLOWED_HOSTS=\* -a careers-prod
+deis domains:add careers.mozilla.com -a careers-prod
 kubectl -n careers-prod apply -f ./k8s/careers-prod-nodeport.yaml
-deis pull mozorg/lumbergh:2b7d32 -a careers-prod
-#
+deis pull mozorg/lumbergh:a66c97 -a careers-prod
