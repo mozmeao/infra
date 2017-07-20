b9da8246e0921dc4d0ff04c31f87c4b567897338
Dave Parfitt <dparfitt@mozilla.com>
2017-07-20 15:27:26 +0000

    whitespace

--- apps/snippets/teardown.sh
+++ apps/snippets/teardown.sh
@@ -8,5 +8,3 @@ deis apps:destroy -a snippets-prod  --confirm snippets-prod
 
 neres delete-monitor $(get_newrelic_monitor_id "Snippets Prod Tokyo")
 neres delete-monitor $(get_newrelic_monitor_id "Snippets Prod Frankfurt")
-
-
