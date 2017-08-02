3ca99f855d5b0fd014eab11397dfea4cd1fbf55a
Dave Parfitt <dparfitt@mozilla.com>
2017-08-02 21:21:19 +0000

    portland provisioning

--- apps/mdn/mdn-aws/infra/multi_region/tf/efs.tf
+++ apps/mdn/mdn-aws/infra/multi_region/tf/efs.tf
@@ -1,22 +1,58 @@
-resource "aws_efs_file_system" "mdn-shared" {
-  performance_mode = "generalPurpose"
-  # also supported, maxIO
+# dev
 
+resource "aws_efs_file_system" "mdn-shared-dev" {
+  performance_mode = "generalPurpose"
   tags {
-    Name = "MDN shared"
+    Name = "mdn-shared-dev"
   }
 }
 
-resource "aws_efs_mount_target" "mdn-shared-mt" {
+resource "aws_efs_mount_target" "mdn-shared-dev-mt" {
   # split the subnet variable into a list, then take the length of the list
   count           = "${length(split(",", var.subnets))}"
   # use the EFS filesystem we created above
-  file_system_id = "${aws_efs_file_system.mdn-shared.id}"
+  file_system_id = "${aws_efs_file_system.mdn-shared-dev.id}"
   # split the subnet variable into a list, then get the count.index subnet id
   subnet_id      = "${element(split(",", var.subnets), count.index)}"
+  security_groups = ["${var.nodes_security_group}"]
 }
 
-output "efs_dns" {
-  # all AZ's return the same dns name, just use the first
-  value = "${aws_efs_mount_target.mdn-shared-mt.0.dns_name}"
+
+# stage
+
+resource "aws_efs_file_system" "mdn-shared-stage" {
+  performance_mode = "generalPurpose"
+  tags {
+    Name = "mdn-shared-stage"
+  }
+}
+
+resource "aws_efs_mount_target" "mdn-shared-stage-mt" {
+  # split the subnet variable into a list, then take the length of the list
+  count           = "${length(split(",", var.subnets))}"
+  # use the EFS filesystem we created above
+  file_system_id = "${aws_efs_file_system.mdn-shared-stage.id}"
+  # split the subnet variable into a list, then get the count.index subnet id
+  subnet_id      = "${element(split(",", var.subnets), count.index)}"
+  security_groups = ["${var.nodes_security_group}"]
+}
+
+
+# prod
+
+resource "aws_efs_file_system" "mdn-shared-prod" {
+  performance_mode = "generalPurpose"
+  tags {
+    Name = "mdn-shared-prod"
+  }
+}
+
+resource "aws_efs_mount_target" "mdn-shared-prod-mt" {
+  # split the subnet variable into a list, then take the length of the list
+  count           = "${length(split(",", var.subnets))}"
+  # use the EFS filesystem we created above
+  file_system_id = "${aws_efs_file_system.mdn-shared-prod.id}"
+  # split the subnet variable into a list, then get the count.index subnet id
+  subnet_id      = "${element(split(",", var.subnets), count.index)}"
+  security_groups = ["${var.nodes_security_group}"]
 }
