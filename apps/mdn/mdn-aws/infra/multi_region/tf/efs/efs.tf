c0bd9674102926376e9ddacfca3a090568c7eb46
Dave Parfitt <dparfitt@mozilla.com>
2017-08-03 18:44:21 +0000

    misc

--- apps/mdn/mdn-aws/infra/multi_region/tf/efs/efs.tf
+++ apps/mdn/mdn-aws/infra/multi_region/tf/efs/efs.tf
@@ -14,7 +14,7 @@ resource "aws_efs_file_system" "mdn-shared-efs" {
 resource "aws_efs_mount_target" "mdn-shared-mt" {
   # split the subnet variable into a list, then take the length of the list
   count           = "${length(split(",", var.subnets))}"
-  ## use the EFS filesystem we created above
+  # use the EFS filesystem we created above
   file_system_id = "${aws_efs_file_system.mdn-shared-efs.id}"
   # split the subnet variable into a list, then get the count.index subnet id
   subnet_id      = "${element(split(",", var.subnets), count.index)}"
