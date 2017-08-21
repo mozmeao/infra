c59a41b2d245fb6b4886abcf2a7713adad34c4e0
Dave Parfitt <dparfitt@mozilla.com>
2017-08-21 19:25:13 +0000

    Cloudfront provisioning for AWS-hosted MDN

---
+++ apps/mdn/mdn-aws/infra/mdn-cdn/mdn_cdn.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mdn-cdn-provisioning-tf-state"
+    key    = "tf-state"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_cloudfront_distribution" "mdn-cf-dist" {
+  aliases = ["cdn.mdn.mozilla.org", "cdn.mdn.moz.works"]
+  comment             = "CDN for AWS-hosted MDN"
+  enabled             = true
+  http_version        = "http2"
+  is_ipv6_enabled     = false
+  price_class         = "PriceClass_All"
+
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 404
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    # 86400 = 24 hours
+    max_ttl                = 31536000
+    # 31536000 = 1 year
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id = "MDNCDN"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  origin {
+    domain_name = "developer.mozilla.org"
+    origin_id   = "MDNCDN"
+    custom_origin_config {
+      http_port = "80"
+      https_port = "443"
+      origin_protocol_policy = "https-only"
+      origin_read_timeout = 30
+      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_keepalive_timeout = 5
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "TODO"
+    ssl_support_method  = "sni-only"
+
+    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
+    minimum_protocol_version = "TLSv1"
+  }
+}
+
