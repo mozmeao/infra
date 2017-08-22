13c50ef1347f47d6e9844f8ea81e643937cda3c4
Dave Parfitt <dparfitt@mozilla.com>
2017-08-22 14:25:47 +0000

    add stage/prod using a module

--- apps/mdn/mdn-aws/infra/mdn-cdn/mdn_cdn.tf
+++ apps/mdn/mdn-aws/infra/mdn-cdn/mdn_cdn.tf
@@ -10,65 +10,29 @@ terraform {
   }
 }
 
-resource "aws_cloudfront_distribution" "mdn-cf-dist" {
-  aliases = ["cdn.mdn.mozilla.net", "cdn.mdn.moz.works"]
-  comment             = "CDN for AWS-hosted MDN"
-  enabled             = true
-  http_version        = "http2"
-  is_ipv6_enabled     = false
-  price_class         = "PriceClass_All"
 
-  custom_error_response {
-    error_caching_min_ttl = 10
-    error_code            = 404
-  }
+module "mdn-cloudfront-stage" {
+    source = "./cloudfront"
 
-  default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    compress               = true
-    default_ttl            = 86400
-    # 86400 = 24 hours
-    max_ttl                = 31536000
-    # 31536000 = 1 year
-    min_ttl                = 0
-    smooth_streaming       = false
-    target_origin_id = "MDNCDN"
-    viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  origin {
-    domain_name = "developer.mozilla.org"
-    origin_id   = "MDNCDN"
-    custom_origin_config {
-      http_port = "80"
-      https_port = "443"
-      origin_protocol_policy = "https-only"
-      origin_read_timeout = 30
-      origin_ssl_protocols = ["TLSv1", "TLSv1.1", "TLSv1.2"]
-      origin_keepalive_timeout = 5
-    }
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn = "TODO"
-    ssl_support_method  = "sni-only"
-
-    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
-    minimum_protocol_version = "TLSv1"
-  }
+    acm_cert_arn = ""
+    aliases = ["stage-cdn.mdn.mozilla.net", "stage-cdn.mdn.moz.works"]
+    comment = "Stage CDN for AWS-hosted MDN"
+    distribution_name = "MDNStageCDN"
+    domain_name = "developer.allizom.org"
 }
+
+
+module "mdn-cloudfront-prod" {
+    source = "./cloudfront"
+
+    acm_cert_arn = ""
+    aliases = ["cdn.mdn.mozilla.net", "cdn.mdn.moz.works"]
+    comment = "Prod CDN for AWS-hosted MDN"
+    distribution_name = "MDNProdCDN"
+    domain_name = "developer.mozilla.org"
+}
+
+
+
+
 
