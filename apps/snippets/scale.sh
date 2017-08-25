e59f298b24d484ed2a02c0ce15df00b0043ec056
Giorgos Logiotatidis <seadog@sealabs.net>
2017-08-25 10:38:48 +0300

    Increase the number of max pods for snippets-prod.

--- apps/snippets/scale.sh
+++ apps/snippets/scale.sh
@@ -5,6 +5,6 @@ check_meao_env
 
 deis limits:set web=300M/600M -a snippets-prod
 deis limits:set web=250m/1000m --cpu -a snippets-prod
-deis autoscale:set web --min=5 --max=10 --cpu-percent=80 -a snippets-prod
+deis autoscale:set web --min=5 --max=15 --cpu-percent=80 -a snippets-prod
 
 deis scale clock=1 -a snippets-prod
