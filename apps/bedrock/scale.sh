1062872b0538b2416c9fc425a90053c7a49e18b2
Dave Parfitt <dparfitt@mozilla.com>
2017-09-05 10:18:19 -0400

    remove explicit bedrock clock proc scaling

--- apps/bedrock/scale.sh
+++ apps/bedrock/scale.sh
@@ -8,22 +8,18 @@ deis limits:set web=250m/1000m --cpu -a bedrock-prod
 deis limits:set clock=300M/600M -a bedrock-prod
 deis limits:set clock=250m/1000m --cpu -a bedrock-prod
 deis autoscale:set web --min=5 --max=20 --cpu-percent=80 -a bedrock-prod
-#deis autoscale:set clock --min=1 --max=1--cpu-percent=80 -a bedrock-prod
-
 
 deis limits:set web=300M/600M -a bedrock-stage
 deis limits:set web=250m/1000m --cpu -a bedrock-stage
 deis limits:set clock=300M/600M -a bedrock-stage
 deis limits:set clock=250m/1000m --cpu -a bedrock-stage
 deis autoscale:set web --min=5 --max=20 --cpu-percent=80 -a bedrock-stage
-#deis autoscale:set clock --min=1 --max=1 --cpu-percent=80 -a bedrock-stage
 
 deis limits:set web=120M/300M -a bedrock-dev
 deis limits:set web=250m/500m --cpu  -a bedrock-dev
 deis limits:set clock=120M/300M -a bedrock-dev
 deis limits:set clock=250m/500m --cpu  -a bedrock-dev
 deis autoscale:set web --min=1 --max=3 --cpu-percent=80 -a bedrock-dev
-#deis autoscale:set clock --min=1 --max=1 --cpu-percent=80 -a bedrock-dev
 
 # scale the clock process
 # this will run DB migrations on it's first run
