18bb51e3e45e4594279830f7a663da53280c70c4
Giorgos Logiotatidis <seadog@sealabs.net>
2017-09-27 15:09:42 +0300

    [careers] Update resource and autoscale limits.
    Reduce reserved resources for careers and still allow to scale up to 5 instances
    if needed. Those limits should be enough according to the current resource
    usage.

--- apps/careers/scale.sh
+++ apps/careers/scale.sh
@@ -3,8 +3,6 @@
 source ../bin/common.sh
 check_meao_env
 
-deis limits:set web=120M/300M -a careers-prod
-deis limits:set web=250m/500m --cpu -a careers-prod
-deis autoscale:set web --min=3 --max=5 --cpu-percent=80 -a careers-prod
-
-
+deis limits:set web=128M/150M -a careers-prod
+deis limits:set web=100m/200m --cpu -a careers-prod
+deis autoscale:set web --min=2 --max=5 --cpu-percent=80 -a careers-prod
