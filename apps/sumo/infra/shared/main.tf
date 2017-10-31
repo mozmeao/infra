6a0a4fe42a03b3a931acc57448f0eaf79e4811b8
Dave Parfitt <dparfitt@mozilla.com>
2017-10-31 18:12:31 +0000

    update certs for SUMO CDN's

--- apps/sumo/infra/shared/main.tf
+++ apps/sumo/infra/shared/main.tf
@@ -48,7 +48,7 @@ module "sumo-user-media-prod-bucket" {
 module "sumo-user-media-dev-cf" {
     source = "./cloudfront"
 
-    acm_cert_arn = "TODO"
+    acm_cert_arn = "arn:aws:acm:us-east-1:236517346949:certificate/d8711106-680f-42d3-b9b5-e10b39381c7e"
     aliases = ["dev-cdn.sumo.mozilla.net", "dev-cdn.sumo.moz.works"]
     comment = "Dev CDN for SUMO user media"
     distribution_name = "SUMOMediaDevCDN"
@@ -58,7 +58,7 @@ module "sumo-user-media-dev-cf" {
 module "sumo-user-media-stage-cf" {
     source = "./cloudfront"
 
-    acm_cert_arn = "TODO"
+    acm_cert_arn = "arn:aws:acm:us-east-1:236517346949:certificate/c882842a-b244-4a32-896a-0b5dace0c3a4"
     aliases = ["stage-cdn.sumo.mozilla.net", "stage-cdn.sumo.moz.works"]
     comment = "Stage CDN for SUMO user media"
     distribution_name = "SUMOMediaStageCDN"
@@ -68,7 +68,7 @@ module "sumo-user-media-stage-cf" {
 module "sumo-user-media-prod-cf" {
     source = "./cloudfront"
 
-    acm_cert_arn = "TODO"
+    acm_cert_arn = "arn:aws:acm:us-east-1:236517346949:certificate/243ac46e-bf75-4daa-b8c1-6043de87959d"
     aliases = ["prod-cdn.sumo.mozilla.net", "prod-cdn.sumo.moz.works"]
     comment = "Prod CDN for SUMO user media"
     distribution_name = "SUMOMediaProdCDN"
