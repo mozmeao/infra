6405747e7ba2c1735fac01ae3a20fbfe5ede9c7a
Giorgos Logiotatidis <glogiotatidis@users.noreply.github.com>
2017-11-01 13:56:38 +0200

    Update snippets autoscale policy. (#634)

--- apps/snippets/scale.sh
+++ apps/snippets/scale.sh
@@ -5,4 +5,4 @@ check_meao_env
 
 deis limits:set web=300M/600M -a snippets-prod
 deis limits:set web=250m/1000m --cpu -a snippets-prod
-deis autoscale:set web --min=10 --max=30 --cpu-percent=80 -a snippets-prod
+deis autoscale:set web --min=3 --max=10 --cpu-percent=80 -a snippets-prod
