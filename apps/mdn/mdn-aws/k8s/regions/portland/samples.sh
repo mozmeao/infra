ec87d7cdcec3f9b90328f3d4e430d50ca6fc2cbe
Josh Mize <jgmize@gmail.com>
2017-11-06 18:12:27 -0600

    Add mdn-samples initial deployment

---
+++ apps/mdn/mdn-aws/k8s/regions/portland/samples.sh
@@ -0,0 +1,7 @@
+export SAMPLES_IMAGE_TAG=7d0a41c
+export SAMPLES_NGINX_IMAGE=quay.io/mozmar/mdn-samples-nginx
+export SAMPLES_WEBRTC_IMAGE=quay.io/mozmar/mdn-samples-webrtc
+export SAMPLES_WEBSOCKET_IMAGE=quay.io/mozmar/mdn-samples-websocket
+export SAMPLES_NGINX_CONTAINER_PORT=80
+export SAMPLES_WEBSOCKET_CONTAINER_PORT=6502
+export SAMPLES_WEBRTC_CONTAINER_PORT=6503
