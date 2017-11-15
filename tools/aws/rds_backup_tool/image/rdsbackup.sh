05f0c323b8242f5d211d7338042d6263161f07a8
Dave Parfitt <diparfitt@gmail.com>
2017-11-15 11:08:56 -0500

    implement K8s-based RDS backup tool for MySQL|MariaDB|Postgres

---
+++ tools/aws/rds_backup_tool/image/rdsbackup.sh
@@ -0,0 +1,159 @@
+#!/bin/bash -e
+
+# env vars:
+# DBTYPE: accepts either of the following values: MYSQL or PGSQL
+# DBNAME: name of database to use in connection string
+# DBUSER: MySQL OR Postgres username
+# DBPASSWORD: MySQL OR Postgres password
+# DBHOST: MySQL OR Postgres host without port#
+# DBPORT: MySQL OR Postgres port #
+# BACKUP_CMD_PARAMS: additional parameters to pass to backup command for psql/mysql
+# BACKUP_BUCKET: where to write backup file, includes s3:// prefix
+# BACKUP_DIR: directory in the container mapped to EBS volume
+# BACKUP_PASSWORD: symmetric encryption password
+# KEEP_N_DAYS: delete files older than KEEP_N_DAYS for a particular DBNAME
+# AWS_ACCESS_KEY_ID
+# AWS_SECRET_ACCESS_KEY
+
+check_requirements() {
+    if [[ -z $DBHOST || -z $DBNAME || -z $DBPASSWORD \
+          || -z $DBPORT || -z $DBTYPE || -z $DBUSER || -z $BACKUP_DIR \
+          || -z $BACKUP_PASSWORD ]]; then
+        echo "ERROR: Not all environment variables are set."
+        echo "Minimum required vars:"
+        echo "  DBHOST"
+        echo "  DBNAME"
+        echo "  DBPASSWORD"
+        echo "  DBPORT"
+        echo "  DBTYPE"
+        echo "  DBUSER"
+        echo "  BACKUP_DIR"
+        echo "  BACKUP_PASSWORD"
+        exit 1
+    fi
+}
+
+fix_creds() {
+    # use a ~/.aws/credentials file INSTEAD of k8s set environment vars.
+    # Unset env vars after the file is written.
+    # Creds containing a slash seem to break the AWS CLI when run inside k8s,
+    # but running in docker is fine.
+    #
+    # example error:
+    # fatal error: Invalid header value b'AWS AKIAXXXX\n:ri81t84+26rX0qzRAASDDDXXXy2B70='
+    mkdir "${HOME}"/.aws
+    cat << EOF > "${HOME}"/.aws/credentials
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
+EOF
+    unset AWS_ACCESS_KEY_ID
+    unset AWS_SECRET_ACCESS_KEY
+}
+
+# remove files for the current DBNAME older than KEEP_N_DAYS
+cleanup_old() {
+    echo "Cleaning up old files in ${BACKUP_OUTPUT_DIR}"
+    echo "Removing the following old local backups:"
+    find ${BACKUP_OUTPUT_DIR} -mtime +${KEEP_N_DAYS} -type f || true
+    find ${BACKUP_OUTPUT_DIR} -mtime +${KEEP_N_DAYS} -type f -delete || true
+    echo "Finished file cleanup"
+}
+
+push_to_s3() {
+    REMOTE_DIR="${BACKUP_BUCKET}/${DBNAME}/"
+    echo "Pushing ${ENCRYPTED_BACKUP_FILENAME} to ${REMOTE_DIR}"
+    aws s3 cp "${ENCRYPTED_BACKUP_FILENAME}" "${REMOTE_DIR}"
+    echo "Finished pushing to S3"
+}
+
+encrypt_backup() {
+    echo "Encrypting ${BACKUP_FILENAME}"
+    echo -n "${BACKUP_PASSWORD}" | \
+        gpg --batch \
+            --passphrase-fd 0 \
+            --symmetric \
+            --cipher-algo aes256 \
+            ${BACKUP_FILENAME}
+    echo "Finished encrypting ${BACKUP_FILENAME}"
+    echo "Removing original: ${BACKUP_FILENAME}"
+    rm ${BACKUP_FILENAME}
+    echo "Finished encrypting"
+}
+
+postgres_backup() {
+    echo "Starting Postgresql backup on $(date)"
+    export PGUSER="${DBUSER}"
+    export PGPASSWORD="${DBPASSWORD}"
+    export PGHOST="${DBHOST}"
+    export PGPORT="${DBPORT}"
+    export PGDATABASE="${DBNAME}"
+    pg_dump "${DBNAME}" | gzip > ${BACKUP_FILENAME}
+    echo "Finished Postgresql backup on $(date)"
+}
+
+mysql_backup() {
+    echo "Starting MySQL backup on $(date)"
+    export MYSQL_PWD="${DBPASSWORD}"
+    mysqldump -u${DBUSER} \
+              -h${DBHOST} \
+              -P${DBPORT} \
+              ${MYSQL_BACKUP_OPTIONS} "${DBNAME}" | gzip > ${BACKUP_FILENAME}
+    echo "Finished MySQL backup on $(date)"
+}
+
+perform_backup() {
+    cd ${BACKUP_DIR}
+    mkdir -p ${DBNAME}
+    cd ${DBNAME}
+
+    echo "Backup output file: ${BACKUP_FILENAME}"
+    echo "Encrypted backup output file: ${ENCRYPTED_BACKUP_FILENAME}"
+
+    if [[ "${DBTYPE}" == "PGSQL" ]]; then
+        postgres_backup
+    elif [[ "${DBTYPE}" == "MYSQL" ]]; then
+        mysql_backup
+    else
+        echo "DBTYPE must be PGSQL or MYSQL"
+        exit 1
+    fi
+}
+
+notify_deadmanssnitch() {
+    if [[ -z "${DEADMANSSNITCH_URL}" ]]; then
+        echo "DEADMANSSNITCH_URL is not configured"
+    else
+        echo "updating Deadmanssnitch: ${DEADMANSSNITCH_URL}"
+        curl "${DEADMANSSNITCH_URL}"
+        echo "Deadmanssnitch updated"
+    fi
+}
+
+export MYSQL_BACKUP_OPTIONS="${BACKUP_CMD_PARAMS:- --dump-date --compress --single-transaction}"
+export PGSQL_BACKUP_OPTIONS="${BACKUP_CMD_PARAMS:- }"
+
+export KEEP_N_DAYS="${KEEP_N_DAYS:-10}"
+
+export BACKUP_OUTPUT_DIR="${BACKUP_DIR}/${DBNAME}"
+export BASE_FILENAME="${DBNAME}.$( date +%F ).sql.gz"
+export BACKUP_FILENAME="${BACKUP_OUTPUT_DIR}/${BASE_FILENAME}"
+export ENCRYPTED_BACKUP_FILENAME="${BACKUP_FILENAME}.gpg"
+
+if [ -e ${ENCRYPTED_BACKUP_FILENAME} ]
+then
+    echo "Encrypted file ${ENCRYPTED_BACKUP_FILENAME} already exists, exiting."
+    exit 1
+fi
+
+echo "${DBNAME} backup started at $(date)"
+check_requirements
+fix_creds
+cleanup_old
+perform_backup
+encrypt_backup
+push_to_s3
+notify_deadmanssnitch
+echo "${DBNAME} backup finished at $(date)"
+
+
