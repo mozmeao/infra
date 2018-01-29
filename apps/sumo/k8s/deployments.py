ca67a93cabcf3d11a9d43085360728f2a230bd07
Dave Parfitt <diparfitt@gmail.com>
2018-01-29 12:45:40 -0500

    autopep8 formatting

--- apps/sumo/k8s/deployments.py
+++ apps/sumo/k8s/deployments.py
@@ -3,23 +3,26 @@ from invoke.exceptions import Exit
 from env import *
 from deploy_utils import render_template, k8s_apply, k8s_delete_resource
 
+
 @task()
 def check_environment(ctx):
     """
     Ensure that a .yaml file has been specified
     """
-    if not 'K8S_NAMESPACE' in ctx.config:
+    if 'K8S_NAMESPACE' not in ctx.config:
         print("Please specify a configuration file with -f")
         raise Exit()
+
 
 @task(check_environment)
 def create_web(ctx, apply=False):
     """
     Create or update a SUMO web deployment
     """
-    t = render_template(config = ctx.config,
+    t = render_template(config=ctx.config,
                         template_name=SUMO_WEB_TEMPLATE)
     k8s_apply(ctx, t, apply)
+
 
 @task(check_environment)
 def delete_web(ctx, apply=False, infra_apply=False):
@@ -32,15 +35,17 @@ def delete_web(ctx, apply=False, infra_apply=False):
         k8s_delete_resource(ctx, deployment, apply)
     else:
         print("Infra tasks require an additional --infra-apply confirmation")
-    
+
+
 @task(check_environment)
 def create_nodeport(ctx, apply=False, infra_apply=False):
     """
     Create or update a SUMO nodeport
     """
-    t = render_template(config = ctx.config,
+    t = render_template(config=ctx.config,
                         template_name=SUMO_NODEPORT_TEMPLATE)
     k8s_apply(ctx, t, apply)
+
 
 @task(check_environment)
 def delete_nodeport(ctx, apply=False, infra_apply=False):
@@ -49,7 +54,6 @@ def delete_nodeport(ctx, apply=False, infra_apply=False):
     """
     if infra_apply:
         deployment = 'deploy/{}'.format(ctx.config['SUMO_NODEPORT_NAME'])
-        k8s_delete_resource(ctx, deployment, apply)    
+        k8s_delete_resource(ctx, deployment, apply)
     else:
         print("Infra tasks require an additional --infra-apply confirmation")
-    
--- apps/sumo/k8s/rollouts.py
+++ apps/sumo/k8s/rollouts.py
@@ -2,15 +2,17 @@ from invoke import task
 from invoke.exceptions import Exit
 from env import *
 
+
 @task
 def check_environment(ctx):
     """
     Ensure that a .yaml file has been specified
     """
     # I'm not sure how to share these between invoke namespaces (yet?)
-    if not 'K8S_NAMESPACE' in ctx.config:
+    if 'K8S_NAMESPACE' not in ctx.config:
         print("Please specify a configuration file with -f")
         raise Exit()
+
 
 @task(check_environment)
 def status_web(ctx):
@@ -19,7 +21,9 @@ def status_web(ctx):
     """
     namespace = ctx.config['K8S_NAMESPACE']
     web_deployment_name = ctx.config['SUMO_WEB_DEPLOYMENT_NAME']
-    ctx.run('kubectl -n {}} rollout status deploy {}'.format(namespace, web_deployment_name))
+    ctx.run('kubectl -n {}} rollout status deploy {}'.format(namespace,
+                                                             web_deployment_name))
+
 
 @task(check_environment)
 def rollback_web(ctx):
@@ -28,4 +32,5 @@ def rollback_web(ctx):
     """
     namespace = ctx.config['K8S_NAMESPACE']
     web_deployment_name = ctx.config['SUMO_WEB_DEPLOYMENT_NAME']
-    ctx.run('kubectl -n {}} rollout undo deploy {}'.format(namespace, web_deployment_name))
+    ctx.run(
+        'kubectl -n {}} rollout undo deploy {}'.format(namespace, web_deployment_name))
