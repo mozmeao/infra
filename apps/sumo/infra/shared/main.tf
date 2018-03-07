4a00ff9c193f23d8348b39e31c713b5f987ac9ff
Dave Parfitt <dparfitt@mozilla.com>
2018-03-07 15:56:29 +0000

    rename SUMO failover CDN

--- apps/sumo/infra/shared/main.tf
+++ apps/sumo/infra/shared/main.tf
@@ -120,7 +120,7 @@ module "sumo-failover-cf" {
     source = "./cloudfront_failover"
 
     acm_cert_arn = "TODO"
-    aliases = ["support.mozilla.org", "support.mozilla.com", "static-failover-cdn.sumo.moz.works"]
+    aliases = ["support.mozilla.org", "support.mozilla.com", "failover-cdn.sumo.moz.works"]
     comment = "Frankfurt failover CDN"
     distribution_name = "SUMOFailoverCDN"
     domain_name = "prod-frankfurt.sumo.moz.works"
