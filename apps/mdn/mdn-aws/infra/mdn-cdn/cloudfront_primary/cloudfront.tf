aa2e18e1c315007da0a5a93524cba5a44f3065a4
rjohnson <rjohnson@mozilla.com>
2018-04-10 16:00:33 -0700

    update primary cdn configuration and add README.md

--- apps/mdn/mdn-aws/infra/mdn-cdn/cloudfront_primary/cloudfront.tf
+++ apps/mdn/mdn-aws/infra/mdn-cdn/cloudfront_primary/cloudfront.tf
@@ -17,6 +17,21 @@ resource "aws_cloudfront_distribution" "mdn-primary-cf-dist" {
 
   custom_error_response {
     error_caching_min_ttl = 0
+    error_code            = 403
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 404
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 0
+    error_code            = 500
+  }
+
+  custom_error_response {
+    error_caching_min_ttl = 0
     error_code            = 504
   }
 
@@ -59,8 +74,8 @@ resource "aws_cloudfront_distribution" "mdn-primary-cf-dist" {
     viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
-      query_string = false
       headers = ["Host"]
+      query_string = false
 
       cookies {
         forward = "none"
@@ -70,9 +85,9 @@ resource "aws_cloudfront_distribution" "mdn-primary-cf-dist" {
 
   # 2
   cache_behavior {
-    path_pattern = "*/docs/*"
+    path_pattern = "*/docs/new"
 
-    allowed_methods        = ["GET", "HEAD"]
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
     cached_methods         = ["GET", "HEAD"]
     compress               = true
     default_ttl            = 86400
@@ -84,17 +99,292 @@ resource "aws_cloudfront_distribution" "mdn-primary-cf-dist" {
 
     forwarded_values {
       query_string = true
-      headers = ["Host"]
-
+      headers = ["*"]
       cookies {
-        forward = "whitelist"
-        whitelisted_names = ["dwf_sg_task_completion", "sessionid"]
+        forward = "all"
       }
     }
   }
 
-
   # 3
+  cache_behavior {
+    path_pattern = "*/docs/preview-wiki-content"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 4
+  cache_behavior {
+    path_pattern = "*/docs/*$edit"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 5
+  cache_behavior {
+    path_pattern = "*/docs/*$subscribe"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 6
+  cache_behavior {
+    path_pattern = "*/docs/*$subscribe_to_tree"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 7
+  cache_behavior {
+    path_pattern = "*/docs/*$translate"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 8
+  cache_behavior {
+    path_pattern = "*/docs/*$quick-review"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 9
+  cache_behavior {
+    path_pattern = "*/docs/*$move"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 10
+  cache_behavior {
+    path_pattern = "*/docs/*$delete"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 11
+  cache_behavior {
+    path_pattern = "*/docs/*$revert/*"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 12
+  cache_behavior {
+    path_pattern = "*/docs/*$purge"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 13
+  cache_behavior {
+    path_pattern = "*/docs/*$files"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 14
+  cache_behavior {
+    path_pattern = "*/docs/*"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["Host", "Accept-Language"]
+
+      cookies {
+        forward = "whitelist"
+        whitelisted_names = ["django_language", "dwf_sg_task_completion", "sessionid"]
+      }
+    }
+  }
+
+  # 15
   cache_behavior {
     path_pattern = "*/dashboards/revisions"
 
@@ -110,39 +400,15 @@ resource "aws_cloudfront_distribution" "mdn-primary-cf-dist" {
 
     forwarded_values {
       query_string = true
-      headers = ["Host", "X-Requested-With"]
+      headers = ["Host", "Accept-Language", "X-Requested-With"]
       cookies {
         forward = "whitelist"
-        whitelisted_names = ["sessionid"]
+        whitelisted_names = ["django_language", "sessionid"]
       }
     }
   }
 
-  # 4
-  cache_behavior {
-    path_pattern = "*/dashboards/user_lookup"
-
-    allowed_methods        = ["GET", "HEAD"]
-    cached_methods         = ["GET", "HEAD"]
-    compress               = true
-    default_ttl            = 86400
-    max_ttl                = 31536000
-    min_ttl                = 0
-    smooth_streaming       = false
-    target_origin_id       = "${var.distribution_name}"
-    viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = true
-      query_string_cache_keys = ["user"]
-      headers = ["Host", "X-Requested-With"]
-      cookies {
-        forward = "none"
-      }
-    }
-  }
-
-  # 5
+  # 16
   cache_behavior {
     path_pattern = "*/dashboards/topic_lookup"
 
@@ -158,16 +424,18 @@ resource "aws_cloudfront_distribution" "mdn-primary-cf-dist" {
 
     forwarded_values {
       query_string = true
-      query_string_cache_keys = ["topic"]
-      headers = ["Host", "X-Requested-With"]
+      headers = ["Host", "Accept-Language", "X-Requested-With"]
       cookies {
-        forward = "none"
+        forward = "whitelist"
+        whitelisted_names = ["django_language"]
       }
     }
   }
 
+  # 17
+  cache_behavior {
+    path_pattern = "*/dashboards/user_lookup"
 
- default_cache_behavior {
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     compress               = true
@@ -180,16 +448,150 @@ resource "aws_cloudfront_distribution" "mdn-primary-cf-dist" {
 
     forwarded_values {
       query_string = true
-      headers = ["Host"]
-
+      headers = ["Host", "Accept-Language", "X-Requested-With"]
       cookies {
         forward = "whitelist"
-        whitelisted_names = ["sessionid"]
+        whitelisted_names = ["django_language"]
       }
     }
   }
 
+  # 18
+  cache_behavior {
+    path_pattern = "*/users/*"
 
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 19
+  cache_behavior {
+    path_pattern = "*/profile/*"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 20
+  cache_behavior {
+    path_pattern = "*/profiles/*"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 21
+  cache_behavior {
+    path_pattern = "*/unsubscribe/*"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  # 22
+  cache_behavior {
+    path_pattern = "admin/*"
+
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["*"]
+      cookies {
+        forward = "all"
+      }
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods        = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods         = ["GET", "HEAD"]
+    compress               = true
+    default_ttl            = 86400
+    max_ttl                = 31536000
+    min_ttl                = 0
+    smooth_streaming       = false
+    target_origin_id       = "${var.distribution_name}"
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = true
+      headers = ["Host", "Accept-Language"]
+
+      cookies {
+        forward = "whitelist"
+        whitelisted_names = ["django_language", "sessionid"]
+      }
+    }
+  }
 
   origin {
     domain_name = "${var.domain_name}"
