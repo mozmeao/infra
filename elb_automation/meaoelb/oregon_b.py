46b77fb54efa3c91816473e27ebfe2ee8102edfd
Josh Mize <jgmize@gmail.com>
2018-04-11 11:07:22 -0500

    Fix variable name: s/stage/prod/

--- elb_automation/meaoelb/oregon_b.py
+++ elb_automation/meaoelb/oregon_b.py
@@ -50,12 +50,12 @@ snippets_stage.elb_config.elb_atts.connection_settings.idle_timeout = 120
 snippets_stage.elb_config.health_check.target_path = '/healthz/'
 
 # ### Snippets Prod
-snippets_stage = elb_tool.define_elb_http(
+snippets_prod = elb_tool.define_elb_http(
     service_namespace='snippets-prod',
     service_name='snippets-nodeport',
     ssl_arn='arn:aws:acm:us-west-2:236517346949:certificate/fbe34166-ae87-43f8-b9cc-7bc9a45d904c')
-snippets_stage.elb_config.elb_atts.connection_settings.idle_timeout = 60
-snippets_stage.elb_config.health_check.target_path = '/healthz/'
+snippets_prod.elb_config.elb_atts.connection_settings.idle_timeout = 60
+snippets_prod.elb_config.health_check.target_path = '/healthz/'
 
 
 # ### SUMO Stage
