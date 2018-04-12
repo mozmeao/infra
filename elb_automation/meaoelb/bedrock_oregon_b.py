eef3e267d26a7f273d577a20f418d7cab4d2fe58
Josh Mize <jgmize@gmail.com>
2018-04-11 21:39:42 -0500

    Refactor oregon-b into separate files for each app

---
+++ elb_automation/meaoelb/bedrock_oregon_b.py
@@ -0,0 +1,19 @@
+from meaoelb.oregon_b import elb_tool
+
+
+bedrock_stage = elb_tool.define_elb(
+    service_namespace='bedrock-stage',
+    service_name='bedrock-nodeport',
+    ssl_arn='arn:aws:acm:us-west-2:236517346949:certificate/657b1ca0-8c09-4add-90a2-1243470a6b45')
+# there are more flexible ways of defining ELBS:
+# elb_tool.cfg_defaults.default_service_config OR
+# elb_tool.cfg_defaults.generic_service_config
+# but elb_tool.define_elb() fills in most of the blanks for you
+
+# add an IdleTimeout as an ELB attribute
+# custom health check configuration
+bedrock_stage.elb_config.health_check.target_path = '/healthz/'
+
+elb_tool.show_elbs()
+elb_tool.test_elbs()
+elb_tool.create_and_bind_elbs()
---
+++ elb_automation/meaoelb/careers_oregon_b.py
@@ -0,0 +1,17 @@
+from meaoelb.oregon_b import elb_tool
+
+careers_stage = elb_tool.define_elb_http(
+    service_namespace='careers-stage',
+    service_name='careers-nodeport',
+    ssl_arn='arn:aws:acm:us-west-2:236517346949:certificate/4952e9c1-dda2-450b-b156-908a42869f4f')
+careers_stage.elb_config.health_check.target_path = '/healthz/'
+
+# show the ELB's before we process them
+# object output is now colorized JSON
+elb_tool.show_elbs()
+
+elb_tool.test_elbs()
+
+# if an ELB has already been created, skip and continue on to the next
+# This also ensures all ELBs are bound to the ASG
+elb_tool.create_and_bind_elbs()
