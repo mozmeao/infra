1f888cb35655d3c4375f5d096947fe3103ddcfb1
Dave Parfitt <dparfitt@mozilla.com>
2018-04-24 17:54:53 +0000

    blockaws cron pings DMS when complete, cleanup build script

--- k8s/tools/block-aws/build.sh
+++ k8s/tools/block-aws/build.sh
@@ -1,5 +1,3 @@
-GIT_COMMIT_RAW=$(git rev-parse HEAD)
-GIT_COMMIT=${GIT_COMMIT_RAW:0:7}
-
-docker build . -t quay.io/mozmar/blockaws:${GIT_COMMIT}
+#!/bin/bash
+docker build . -t quay.io/mozmar/blockaws:${GIT_COMMIT:=$(git rev-parse --short HEAD)}
 docker push quay.io/mozmar/blockaws:${GIT_COMMIT}
