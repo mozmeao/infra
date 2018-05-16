10624b4a79e0638169a951469635a857324bea08
Dave Parfitt <dparfitt@mozilla.com>
2018-05-16 20:32:22 +0000

    remove env stuff, it's in the way!

--- apps/protocol/tf/provision.sh
+++ apps/protocol/tf/provision.sh
@@ -5,20 +5,13 @@ set -u
 
 PROTOCOL_PROVISIONING_REGION="us-west-2"
 TERRAFORM_ENV="protocol"
-PROTOCOL_PROVISIONING_BUCKET="protocol-s3-provisioning-tf-state"
+PROTOCOL_PROVISIONING_BUCKET="protocol-provisioning-tf-state"
 STATE_BUCKET_REGION="us-west-2"
 
 setup_tf_s3_state_store() {
     echo "Creating Terraform state bucket at s3://${PROTOCOL_PROVISIONING_BUCKET} (region ${STATE_BUCKET_REGION})"
     # The following environment variables are defined in config.sh
     aws s3 mb s3://${PROTOCOL_PROVISIONING_BUCKET} --region ${STATE_BUCKET_REGION}
-}
-
-setup_tf_envs() {
-    # this MUST be run in the dir that this file resides in
-    set +e
-    terraform env new protocol
-    set -e
 }
 
 check_state_store() {
@@ -29,8 +22,6 @@ check_state_store() {
     else
         echo "Setting up state store"
         setup_tf_s3_state_store
-        echo "Setting up envs"
-        setup_tf_envs
     fi
     set -e
 }
@@ -38,11 +29,6 @@ check_state_store() {
 tf_main() {
     # it's safe to always init the s3 backend
     terraform init
-
-    setup_tf_envs
-
-    # switch env to virginia, tokyo etc
-    terraform env select ${TERRAFORM_ENV}
 
     # import local modules
     terraform get
