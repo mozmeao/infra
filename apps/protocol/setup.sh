23637b4ee5910c83e32f30b01fa94857fa36f392
Dave Parfitt <diparfitt@gmail.com>
2018-06-07 14:53:57 -0400

    viewsource -> protocol

--- apps/protocol/setup.sh
+++ apps/protocol/setup.sh
@@ -3,8 +3,8 @@
 source ../bin/common.sh
 check_neres_env
 
-neres add-monitor "Viewsourceconf prod" \
-    https://viewsourceconf.org \
+neres add-monitor "Protocol prod" \
+    https://protocol.mozilla.org \
     --location AWS_US_WEST_2 \
     --frequency 5 \
     --email "${NERES_EMAIL_1}" \
--- apps/protocol/teardown.sh
+++ apps/protocol/teardown.sh
@@ -3,6 +3,6 @@
 source ../bin/common.sh
 check_neres_env
 
-monitor_id=$(get_newrelic_monitor_id "protocol prod")
+monitor_id=$(get_newrelic_monitor_id "Protocol prod")
 neres delete-monitor $monitor_id
 
