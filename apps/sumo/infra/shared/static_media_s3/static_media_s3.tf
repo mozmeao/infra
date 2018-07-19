4bb7340cd6d27e7eab42cfdbbd6e6ea68c621154
Dave Parfitt <diparfitt@gmail.com>
2018-07-19 16:34:11 -0400

    add hosted-zone-id lookup

--- apps/sumo/infra/shared/static_media_s3/static_media_s3.tf
+++ apps/sumo/infra/shared/static_media_s3/static_media_s3.tf
@@ -3,6 +3,15 @@ variable "iam_policy_name" {}
 variable "logging_bucket_id" {}
 variable "logging_prefix" {}
 variable "region" {}
+variable "hosted-zone-id-defs" {
+  # See: https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_website_region_endpoints
+  type = "map"
+
+  default = {
+    us-east-1 = "Z3AQBSTGFYJSTF"
+    us-west-2 = "Z3BJ6K6RIION7M"
+  }
+}
 
 
 resource "aws_s3_bucket" "sumo-prod-media" {
