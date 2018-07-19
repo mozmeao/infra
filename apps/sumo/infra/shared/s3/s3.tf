85dbeba78198f4e6843f7eb3acac2f9508947239
Dave Parfitt <diparfitt@gmail.com>
2018-07-19 16:17:50 -0400

    sumo media buckets
    https://github.com/mozmeao/infra/issues/890

--- apps/sumo/infra/shared/s3/s3.tf
+++ apps/sumo/infra/shared/s3/s3.tf
@@ -67,3 +67,123 @@ resource "aws_s3_bucket" "sumo-user-media" {
 }
 EOF
 }
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "sumo-media-logs"
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket" "sumo-stage-media" {
+  bucket = "sumo-stage-media"
+  region = "${var.region}"
+  acl    = "log-delivery-write"
+
+  force_destroy = ""
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
+  hosted_zone_id = "${lookup(var.hosted-zone-id-defs, var.region)}"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.id}"
+    target_prefix = "stage_logs/"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  website_domain   = "s3-website-${var.region}.amazonaws.com"
+  website_endpoint = "sumo-stage-media.s3-website-${var.region}.amazonaws.com"
+
+  versioning {
+    enabled = true
+  }
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "sumo-stage-media policy",
+  "Statement": [
+    {
+      "Sid": "SumoStageMediaAllowListBucket",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::sumo-stage-media"
+    },
+    {
+      "Sid": "SumoStageMediaAllowIndexDotHTML",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::sumo-stage-media/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "sumo-prod-media" {
+  bucket = "sumo-prod-media"
+  region = "${var.region}"
+  acl    = "log-delivery-write"
+
+  force_destroy = ""
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
+  hosted_zone_id = "${lookup(var.hosted-zone-id-defs, var.region)}"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.id}"
+    target_prefix = "prod_logs/"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+
+  website_domain   = "s3-website-${var.region}.amazonaws.com"
+  website_endpoint = "sumo-prod-media.s3-website-${var.region}.amazonaws.com"
+
+  versioning {
+    enabled = true
+  }
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "sumo-prod-media policy",
+  "Statement": [
+    {
+      "Sid": "SumoProdMediaAllowListBucket",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::sumo-prod-media"
+    },
+    {
+      "Sid": "SumoProdMediaAllowIndexDotHTML",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::sumo-prod-media/*"
+    }
+  ]
+}
+EOF
+}
+
