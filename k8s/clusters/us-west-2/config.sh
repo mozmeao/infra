f346e41645032d4cdd78a9614904d47705e26688
limed <limed@mozilla.com>
2018-08-08 16:22:42 -0700

    Out folder rename

--- k8s/clusters/us-west-2/config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ../common.sh
-
-export KOPS_REGION="us-west-2"
-export KOPS_CLUSTER="k8s.${KOPS_REGION}.${KOPS_DOMAIN}"
-
-export KOPS_MASTER_ZONE="us-west-2a,us-west-2b,us-west-2c"
-export KOPS_ZONE="us-west-2a,us-west-2b,us-west-2c"
