6f45e88a11364ab8b3a56185655afe8ca2a15a1c
Giorgos Logiotatidis <seadog@sealabs.net>
2019-01-09 11:34:53 +0200

    [Fix #997] Careers Static Terraform
    Creates
     - prod bucket
     - staging bucket
     - logs bucket
     - IAM user with S3 permissions to the buckets above
     - CF distribution for staging
     - CF distribution for prod

---
+++ apps/careers/tf/careers.tf
@@ -0,0 +1,283 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "mozilla-careers-s3-provisioning-tf-state"
+    key    = "tf-state"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_iam_user" "careers" {
+  name = "careers-upload"
+}
+
+resource "aws_iam_user_policy" "careers-upload" {
+  name = "careers-upload"
+  user = "${aws_iam_user.careers.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::mozilla-careers"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::mozilla-careers/*"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::mozilla-careers-stage"
+            ]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": [
+                "arn:aws:s3:::mozilla-careers-stage/*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "logs" {
+  bucket = "mozilla-careers-logs"
+  acl    = "log-delivery-write"
+}
+
+resource "aws_s3_bucket" "mozilla-careers" {
+  bucket = "mozilla-careers"
+  region = "${var.region}"
+  acl    = "log-delivery-write"
+
+  force_destroy = ""
+
+  hosted_zone_id = "${lookup(var.hosted-zone-id-defs, var.region)}"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.bucket}"
+    target_prefix = "logs/"
+  }
+
+  website {
+    index_document = "index.html"
+  }
+
+  website_domain   = "s3-website-${var.region}.amazonaws.com"
+  website_endpoint = "mozilla-careers.s3-website-${var.region}.amazonaws.com"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "careers policy",
+  "Statement": [
+    {
+      "Sid": "careersAllowListBucket",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::mozilla-careers"
+    },
+    {
+      "Sid": "careersAllowIndexDotHTML",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::mozilla-careers/*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "mozilla-careers-stage" {
+  bucket = "mozilla-careers-stage"
+  region = "${var.region}"
+  acl    = "log-delivery-write"
+
+  force_destroy = ""
+
+  hosted_zone_id = "${lookup(var.hosted-zone-id-defs, var.region)}"
+
+  logging {
+    target_bucket = "${aws_s3_bucket.logs.bucket}"
+    target_prefix = "stage-logs/"
+  }
+
+  website {
+    index_document = "index.html"
+  }
+
+  website_domain   = "s3-website-${var.region}.amazonaws.com"
+  website_endpoint = "mozilla-careers-stage.s3-website-${var.region}.amazonaws.com"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "careers stage policy",
+  "Statement": [
+    {
+      "Sid": "careersStageAllowListBucket",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:ListBucket",
+      "Resource": "arn:aws:s3:::mozilla-careers-stage"
+    },
+    {
+      "Sid": "careersStageAllowIndexDotHTML",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::mozilla-careers-stage/*"
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "mozilla-careers.s3-website-us-west-2.amazonaws.com"
+    origin_id   = "mozilla-careers"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "No comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "mozilla-careers-logs.s3.amazonaws.com"
+    prefix          = "cflogs"
+  }
+
+  aliases = ["careers.mozilla.org"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "mozilla-careers"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 60
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    # TODO
+    # acm_certificate_arn = "arn:aws:acm:us-west-2:236517346949:certificate/ae2d2e64-b345-4bc1-a49a-5860c73a0809"
+    cloudfront_default_certificate = true
+    ssl_support_method  = "sni-only"
+
+    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
+    minimum_protocol_version = "TLSv1"
+  }
+}
+
+
+resource "aws_cloudfront_distribution" "stage_s3_distribution" {
+  origin {
+    domain_name = "mozilla-careers-stage.s3-website-us-west-2.amazonaws.com"
+    origin_id   = "mozilla-careersstage"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "No comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "mozilla-careers-logs.s3.amazonaws.com"
+    prefix          = "cflogs-stage"
+  }
+
+  aliases = ["careers.allizom.org"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "mozilla-careersstage"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 60
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    # TODO
+    # acm_certificate_arn = "arn:aws:acm:us-west-2:236517346949:certificate/e3d6aa9a-0a9b-4fed-b2fa-d05ae0156677"
+    cloudfront_default_certificate = true
+    ssl_support_method  = "sni-only"
+
+    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
+    minimum_protocol_version = "TLSv1"
+  }
+}
