0cfbcbd97af787360c240934bafd320ce6ac440d
Dave Parfitt <metadave@protonmail.com>
2019-05-01 10:59:55 -0400

    irlpodcast lambda cleanup

---
+++ apps/irlpodcast/tf/cloudfront.tf
@@ -0,0 +1,183 @@
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = "irlpodcast.s3-website-us-west-2.amazonaws.com"
+    origin_id   = "IRLPodcast"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "No comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "irlpodcast-logs.s3.amazonaws.com"
+    prefix          = "cflogs"
+  }
+
+  aliases = ["irlpodcast.org"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "IRLPodcast"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 60
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:236517346949:certificate/dfb1a424-f70d-4c5c-81c6-dd174f455830"
+    ssl_support_method  = "sni-only"
+
+    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
+    minimum_protocol_version = "TLSv1"
+  }
+}
+
+resource "aws_cloudfront_distribution" "www_s3_distribution" {
+  origin {
+    domain_name = "irlpodcast.s3-website-us-west-2.amazonaws.com"
+    origin_id   = "WWWIRLPodcast"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "No comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "irlpodcast-logs.s3.amazonaws.com"
+    prefix          = "cflogs_www"
+  }
+
+  aliases = ["www.irlpodcast.org"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "WWWIRLPodcast"
+
+    forwarded_values {
+      query_string = true
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 60
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:236517346949:certificate/dfb1a424-f70d-4c5c-81c6-dd174f455830"
+    ssl_support_method  = "sni-only"
+
+    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
+    minimum_protocol_version = "TLSv1"
+  }
+}
+
+
+resource "aws_cloudfront_distribution" "stage_s3_distribution" {
+  origin {
+    domain_name = "irlpodcast-stage.s3-website-us-west-2.amazonaws.com"
+    origin_id   = "IRLPodcastStage"
+    custom_origin_config {
+      origin_protocol_policy = "http-only"
+      http_port = "80"
+      https_port = "443"
+      origin_ssl_protocols = ["TLSv1"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "No comment"
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "irlpodcast-logs.s3.amazonaws.com"
+    prefix          = "cflogs-stage"
+  }
+
+  aliases = ["stage.irlpodcast.org"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "IRLPodcastStage"
+
+    lambda_function_association {
+          event_type = "viewer-response"
+          lambda_arn = "${aws_lambda_function.stage-lambda-headers.qualified_arn}"
+    }
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 60
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:236517346949:certificate/841fb67c-df15-44ab-90cd-0fc2a51115a6"
+    ssl_support_method  = "sni-only"
+
+    # https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html#minimum_protocol_version
+    minimum_protocol_version = "TLSv1"
+  }
+}
---
+++ apps/irlpodcast/tf/lambda.tf
@@ -0,0 +1,105 @@
+# Lambda@edge to set origin response headers
+resource "aws_iam_role" "lambda-edge-role" {
+  name = "irlpodcast-lambda-exec-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": [
+          "lambda.amazonaws.com",
+          "edgelambda.amazonaws.com"
+       ]
+     },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+// Stage and prod are pointing at the same javascript file.
+// Change this if we want to deploy stage separately for 
+// development.
+# data "archive_file" "prod-lambda-zip" {
+#   type        = "zip"
+#   source_file = "${path.module}/lambda-headers.js"
+#   output_path = "${path.module}/prod-lambda-headers.zip"
+# }
+
+data "archive_file" "stage-lambda-zip" {
+  type        = "zip"
+  source_file = "${path.module}/lambda-headers.js"
+  output_path = "${path.module}/stage-lambda-headers.zip"
+}
+
+# data "archive_file" "www-prod-lambda-zip" {
+#   type        = "zip"
+#   source_file = "${path.module}/www-lambda.js"
+#   output_path = "${path.module}/www-lambda.zip"
+# }
+
+
+provider "aws" {
+  alias  = "aws-lambda-east"
+  region = "us-east-1"
+}
+
+resource "aws_lambda_function" "stage-lambda-headers" {
+  provider         = "aws.aws-lambda-east"
+  function_name    = "irlpodcast-stage-resp-headers"
+  description      = "Provides correct response Headers for irlpodcast stage"
+  publish          = "true"
+  filename         = "${path.module}/stage-lambda-headers.zip"
+  source_code_hash = "${data.archive_file.stage-lambda-zip.output_base64sha256}"
+  role             = "${aws_iam_role.lambda-edge-role.arn}"
+  handler          = "lambda-headers.handler"
+  runtime          = "nodejs8.10"
+
+  tags {
+    Name        = "irlpodcast-stage-headers"
+    ServiceName = "irlpodcast stage"
+    Terraform   = "true"
+  }
+}
+
+
+# resource "aws_lambda_function" "prod-lambda-headers" {
+#   provider         = "aws.aws-lambda-east"
+#   function_name    = "irlpodcast-prod-resp-headers"
+#   description      = "Provides correct response Headers for irlpodcast prod"
+#   publish          = "true"
+#   filename         = "${path.module}/prod-lambda-headers.zip"
+#   source_code_hash = "${data.archive_file.prod-lambda-zip.output_base64sha256}"
+#   role             = "${aws_iam_role.lambda-edge-role.arn}"
+#   handler          = "lambda-headers.handler"
+#   runtime          = "nodejs8.10"
+
+#   tags {
+#     Name        = "irlpodcast-prod-headers"
+#     ServiceName = "irlpodcast prod"
+#     Terraform   = "true"
+#   }
+# }
+
+# resource "aws_lambda_function" "www-prod-lambda" {
+#   provider         = "aws.aws-lambda-east"
+#   function_name    = "irlpodcast-prod-www"
+#   description      = "Redirect www.irlpodcast.org requests to webpodcast.org"
+#   publish          = "true"
+#   filename         = "${path.module}/www-lambda.zip"
+#   source_code_hash = "${data.archive_file.www-prod-lambda-zip.output_base64sha256}"
+#   role             = "${aws_iam_role.lambda-edge-role.arn}"
+#   handler          = "www-lambda.handler"
+#   runtime          = "nodejs8.10"
+
+#   tags {
+#     Name        = "irlpodcast-www"
+#     ServiceName = "irlpodcast www"
+#     Terraform   = "true"
+#   }
+# }
